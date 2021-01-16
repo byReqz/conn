3f84fecc1437cec865662e58917bf337e063f3c9
Nils <32552517+byReqz@users.noreply.github.com>
2021-01-16 01:38:25 +0000

    tweaked ipv6 stuff and other args

--- conn.sh
+++ conn.sh
@@ -17,17 +17,21 @@ while [ ! -z "$1" ]; do
       echo "Options:"
       echo " -m/--multi -- test multiple ips / disable portscan"
       echo " -h/--help -- show help"
+      echo " -6/--force-ipv6 -- force ipv6 portscanning (also forces portscanning)"
+      echo " -y/--yes -- portscan without asking"
+      echo " -n/--no -- dont portscan"
+      echo " -p/--portscan -- same as -y"
       exit
    elif [[ $1 == "-m" ]] || [[ "$1" == "--multi" ]];then
       echo "multi-ip mode, portscan disabled"
       echo "-------------------Availability----------------------"
-      fping $@
+      fping -e $@
       echo "-----------------------------------------------------"
       exit
    elif [[ $1 == "-y" ]] || [[ $1 == "-p" ]] || [[ "$1" == "--portscan" ]] || [[ "$1" == "--yes" ]];then
       echo "checking connection status for $2"
       echo "-------------------Availability----------------------"
-      fping $2
+      fping -e $2
       echo "-----------------------------------------------------"
       echo "-------------------Portscan---------------------"
       nmap -Pn $2
@@ -35,37 +39,37 @@ while [ ! -z "$1" ]; do
       echo "------------------------------------------------"
       exit
    elif [[ $1 == "-n" ]] || [[ "$1" == "--no" ]];then
-    echo "checking connection status for $2"
-    echo "-------------------Availability----------------------"
-    fping $2
-    echo "-----------------------------------------------------"
-    exit
+      echo "checking connection status for $2"
+      echo "-------------------Availability----------------------"
+      fping -e $2
+      echo "-----------------------------------------------------"
+      exit
    elif [[ $1 == "-6" ]] || [[ "$1" == "--force-ipv6" ]];then
-    echo "-6 used, forcing IPv6 portscanning"
-    echo "checking connection status for $2"
-    echo "-------------------Availability----------------------"
-    fping $2
-    echo "-----------------------------------------------------"
-    echo "-------------------Portscan---------------------"
-    nmap -Pn -6 $2
-    fping -c 4 -A $2
-    echo "------------------------------------------------"
-    exit
+      echo "-6 used, forcing IPv6 portscanning"
+      echo "checking connection status for $2"
+      echo "-------------------Availability----------------------"
+      fping -6 -e $2
+      echo "-----------------------------------------------------"
+      echo "-------------------Portscan---------------------"
+      nmap -Pn -6 $2
+      fping -6 -c 4 -A $2
+      echo "------------------------------------------------"
+      exit
    elif [[ ! "$1" =~ [0-9]{1,3}(\.[0-9]{1,3}){3} ]];then
-    echo "noticed IPv6 adress -> using -6"
-    echo "checking connection status for $1"
-    echo "-------------------Availability----------------------"
-    fping $1
-    echo "-----------------------------------------------------"
-    echo "-------------------Portscan---------------------"
-    nmap -Pn -6 $1
-    fping -c 4 -A $1
-    echo "------------------------------------------------"
-    exit
+      echo "noticed IPv6 adress -> using -6"
+      echo "checking connection status for $1"
+      echo "-------------------Availability----------------------"
+      fping -6 -e $1
+      echo "-----------------------------------------------------"
+      echo "-------------------Portscan---------------------"
+      nmap -Pn -6 $1
+      fping -6 -c 4 -A $1
+      echo "------------------------------------------------"
+      exit
    else
       echo "checking connection status for $1"
       echo "-------------------Availability----------------------"
-      fping $1
+      fping -e $1
       echo "-----------------------------------------------------"
       echo "portscan? (y/n) (default: y)"
       read portscan
